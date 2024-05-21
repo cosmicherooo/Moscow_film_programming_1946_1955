b8687a876e576f27b1100e99a126c0d273ac98d5
cosmichero <ivan.karnauhov.99@gmail.com>
2024-05-22 01:35:14 +0300

    Update parser_script.py

--- imdb_parsing/parser_script.py
+++ imdb_parsing/parser_script.py
@@ -1,1 +1,40 @@
-import 
+import pandas as pd
+import random
+import time
+from imdb_class_parser import MovieInfoIMDb
+
+folder_to_save = '/'
+
+test_df = pd.read_csv("/content/film_test.csv")
+
+test_df["Directors"] = None
+test_df["Writers"] = None
+test_df["Actors"] = None
+test_df["Country of origin"] = None
+test_df["Duration (min)"] = None
+
+for i in range(0, test_df.shape[0]):
+
+  imdb_url = test_df['IMDB_id'][i]
+  film_id = test_df['film_id'][i]
+
+  film_in_cycle = MovieInfoIMDb(film_id, imdb_url)
+  film_in_cycle.reading_html()
+  film_in_cycle.writing_html_into_txt(folder_to_save)
+
+  film_in_cycle.add_actors_names()
+  film_in_cycle.add_country_of_origin()
+  film_in_cycle.add_directors_names()
+  film_in_cycle.add_film_duration()
+  film_in_cycle.add_writers_names()
+
+  test_df['Directors'][i] = film_in_cycle.directors_names
+  test_df['Writers'][i] = film_in_cycle.writers_names
+  test_df['Actors'][i] = film_in_cycle.actors_names
+  test_df['Country of origin'][i] = film_in_cycle.country_of_origin
+  test_df['Duration (min)'][i] = film_in_cycle.duration
+
+
+  value = random.randint(0, 10)
+  time.sleep(value)
+
