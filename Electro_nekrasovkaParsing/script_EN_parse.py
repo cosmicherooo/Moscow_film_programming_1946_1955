7190b209ff1d041af2c89b2e758a3b3f1c743d29
cosmichero <ivan.karnauhov.99@gmail.com>
2024-05-26 17:39:41 +0300

    Update script_EN_parse.py

--- Electro_nekrasovkaParsing/script_EN_parse.py
+++ Electro_nekrasovkaParsing/script_EN_parse.py
@@ -1,8 +1,8 @@
 import ElectroNekrasokaParser from ElectroNekrasokaParser
 
 VM_1946_1955 = ElectroNekrasokaParser('Vecherniya Moscva', '1', list(range(1946, 1956)))
-all_issues_VM = VM_1946_1955.links_to_images(page_num = '4')
 
+all_issues_VM = VM_1946_1955.links_to_images(page_num = '4')
 
 all_issues_VM_1946_1955_df = pd.DataFrame(all_issues_VM, columns = ['issue', 'link_to_page', 'link_to_image_with_programme'])
 
@@ -17,22 +17,24 @@ all_issues_VM_1946_1955_df['year'] = all_issues_VM_1946_1955_df['year'].apply(la
 all_issues_VM_1946_1955_df[['day', 'month']] = all_issues_VM_1946_1955_df.date.str.extract(r'([0-9]{1,2}) ([А-Яа-я]{1,})', expand=True)
 
 
-dict_month = {'1': 'января',
-              '2': 'февраля',
-              '3': 'марта',
-              '4': 'апреля',
-              '5': 'мая',
-              '6': 'июня',
-              '7': 'июля',
-              '8': 'августа',
-              '9': 'сентября',
-              '10': 'октября',
-              '11': 'ноября',
-              '12': 'декабря'}
+dict_month = {'января': '1',
+              'февраля': '2',
+              'марта': '3',
+              'апреля': '4',
+              'мая': '5',
+              'июня': '6',
+              'июля': '7',
+              'августа': '8',
+              'сентября': '9',
+              'октября': '10',
+              'ноября': '11',
+              'декабря': '12'}
 
 
 all_issues_VM_1946_1955_df = all_issues_VM_1946_1955_df.replace({"month": dict_month})
 all_issues_VM_1946_1955_df["year_day"] = all_issues_VM_1946_1955_df["year"] + '/' + all_issues_VM_1946_1955_df["month"] + '/' + all_issues_VM_1946_1955_df["day"]
 all_issues_VM_1946_1955_df.drop(['date', 'year', 'day', 'month'], axis=1, inplace=True)
 
+# сохраняем датасет, затем к нему будут присоединена вручную колонка о том, была ли в газете опублкиована киноафиша
+# 1 - была, 0 - не была
 all_issues_VM_1946_1955_df.to_csv('all_issues_VM_1946_1955_df.csv', sep=',', index=False, encoding='utf-8')
