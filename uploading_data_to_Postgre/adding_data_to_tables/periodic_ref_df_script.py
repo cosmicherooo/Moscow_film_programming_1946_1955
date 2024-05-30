2c9012f208b467c1c230328c82bac4ed70b07351
cosmichero <ivan.karnauhov.99@gmail.com>
2024-05-31 02:16:10 +0300

    Update periodic_ref_df_script.py

--- uploading_data_to_Postgre/adding_data_to_tables/periodic_ref_df_script.py
+++ uploading_data_to_Postgre/adding_data_to_tables/periodic_ref_df_script.py
@@ -5,7 +5,7 @@ from def_filling_tables import *
 import numpy as np
 
 periodic_ref_dataframe_to_load = pd.read_csv(
-    '/Users/karnaukhovivan/Desktop/ВКР_!!!текст_текст/что будет на гите/предобработка/предобработка_газеты/periodic_ref.csv',
+    'pre_processing/pre_rpocessing_periodics/periodic_ref.csv',
     encoding='utf8')
 periodic_ref_dataframe_to_load = periodic_ref_dataframe_to_load.replace(np.nan, None)
 periodic_ref_dataframe_to_load["is_programme"] = periodic_ref_dataframe_to_load["is_programme"].values.astype(str)
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     filling_tables(hostname='localhost',
                    database='Film_Programming_Moscow_1946_1955',
                    username='postgres',
-                   password='85FibanironibI27',
+                   password='********',
                    port_id=5432,
                    insert_script=periodic_ref_insertion_command,
                    data_frame_to_upload=periodic_ref_dataframe_to_load)
