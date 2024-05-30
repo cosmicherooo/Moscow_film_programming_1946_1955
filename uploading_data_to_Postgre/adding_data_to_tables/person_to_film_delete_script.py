207b7b1b69268db28dd50095a6ddbf98f34c2ace
cosmichero <ivan.karnauhov.99@gmail.com>
2024-05-31 02:17:46 +0300

    Update person_to_film_delete_script.py

--- uploading_data_to_Postgre/adding_data_to_tables/person_to_film_delete_script.py
+++ uploading_data_to_Postgre/adding_data_to_tables/person_to_film_delete_script.py
@@ -1,21 +1,25 @@
-from delete_function import *
+from creating_db_template_in_postgres import *
+from creating_tables_db import *
+from creating_execution_commands import *
+from def_filling_tables import *
+import numpy as np
 
-# очищаем всю таблицу
-conn = psycopg2.connect(host=hostname,
-                        dbname=database,
-                        user=username,
-                        password=password,
-                        port=port_id)
-cur = conn.cursor()
-cur.execute('SELECT DISTINCT person_occ FROM person_to_film')
-list_of_person_to_film_ids = [item[0] for item in cur.fetchall()]
-cur.close()
-conn.close()
-print(list_of_person_to_film_ids)
+person_to_film_dataframe_to_load = pd.read_csv(
+    'pre_processing/pre_processing_persons/person_fo_film.csv',
+    encoding='utf8')
+person_to_film_dataframe_to_load = person_to_film_dataframe_to_load.replace(np.nan, None)
+
+person_to_film_insertion_command = tables_insertion_commands.get("person_to_film")
+
+print(person_to_film_insertion_command)
+
 
 if __name__ == '__main__':
-    person_to_film_rows = deleting_by_id(table_name = 'person_to_film',
-                                         column_id_name='person_occ',
-                                         row_id_list=list_of_person_to_film_ids)
-
+    filling_tables(hostname='localhost',
+                   database='Film_Programming_Moscow_1946_1955',
+                   username='postgres',
+                   password='************',
+                   port_id=5432,
+                   insert_script=person_to_film_insertion_command,
+                   data_frame_to_upload=person_to_film_dataframe_to_load)
 
