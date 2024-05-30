513695147ae90b8cb42e8cef76d5101491b085c9
cosmichero <ivan.karnauhov.99@gmail.com>
2024-05-31 02:21:53 +0300

    Update Screening_df_script.py

--- uploading_data_to_Postgre/adding_data_to_tables/Screening_df_script.py
+++ uploading_data_to_Postgre/adding_data_to_tables/Screening_df_script.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     filling_tables(hostname='localhost',
                    database='Film_Programming_Moscow_1946_1955',
                    username='postgres',
-                   password='85FibanironibI27',
+                   password=str(input()),
                    port_id=5432,
                    insert_script=screening_dataframe_to_load_insertion_command,
                    data_frame_to_upload=screening_dataframe_to_load)
