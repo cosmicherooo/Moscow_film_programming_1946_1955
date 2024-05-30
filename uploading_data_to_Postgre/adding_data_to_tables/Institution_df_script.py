3704d951353f7dc0e27c65aa450fb2de29037204
cosmichero <ivan.karnauhov.99@gmail.com>
2024-05-31 02:11:06 +0300

    Update Institution_df_script.py

--- uploading_data_to_Postgre/adding_data_to_tables/Institution_df_script.py
+++ uploading_data_to_Postgre/adding_data_to_tables/Institution_df_script.py
@@ -5,7 +5,7 @@ from def_filling_tables import *
 import numpy as np
 
 institution_dataframe_to_load = pd.read_csv(
-    '/Users/karnaukhovivan/Desktop/ВКР_!!!текст_текст/что будет на гите/предобработка/предобработка_кинотеатры/Institution.csv',
+    'pre_processing/pre_processing_cinemas/Institution.csv',
     encoding='utf8')
 institution_dataframe_to_load = institution_dataframe_to_load.replace(np.nan, None)
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     filling_tables(hostname='localhost',
                    database='Film_Programming_Moscow_1946_1955',
                    username='postgres',
-                   password='85FibanironibI27',
+                   password='***********',
                    port_id=5432,
                    insert_script=institution_insertion_command,
                    data_frame_to_upload=institution_dataframe_to_load)
